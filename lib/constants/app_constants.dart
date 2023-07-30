6e64044d56ec24766dec5918da76fc15a1f19216
Graham Williams <Graham.Williams@togaware.com>
2023-07-31 08:49:53 +1000

    Initial import.

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,21 @@
+/// Constants used throughout the application.
+///
+/// Copyright (C) 2023,  Graham Williams
+///
+/// License: https://www.gnu.org/licenses/quick-guide-gplv3.html
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+/// Authors: Graham Williams
+
+/// The application title as displayed in the desktop window title.
+
+const String APP_TITLE = 'The Flutter Experience';
