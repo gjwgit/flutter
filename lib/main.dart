c70a80a3bc79ffcc2a20b8a356acd0e404633826
Graham Williams <Graham.Williams@togaware.com>
2023-07-31 08:50:04 +1000

    Update copyright. Add constants.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 /// Entry point for the application.
 ///
-/// Copyright 2023, Software Innovation Institute, ANU.
+/// Copyright (C) 2023,  Graham Williams
 ///
-/// License: http://www.apache.org/licenses/LICENSE-2.0
+/// License: https://www.gnu.org/licenses/quick-guide-gplv3.html
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-///
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+// details.
+//
+//
 /// Authors: Graham Williams
 
+import 'package:experience/constants/app_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'; // defaultTargetPlatform
 import 'package:window_size/window_size.dart'; // setWindowTitle()
@@ -39,7 +42,7 @@ class Learner extends StatelessWidget {
     if (defaultTargetPlatform == TargetPlatform.linux ||
         defaultTargetPlatform == TargetPlatform.macOS ||
         defaultTargetPlatform == TargetPlatform.windows) {
-      setWindowTitle('APP_TITLE');
+      setWindowTitle(APP_TITLE);
     }
 
     return MaterialApp(
