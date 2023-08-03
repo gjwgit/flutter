48b77cb70937d963a1f06220b26cc9566446a50c
Graham Williams <Graham.Williams@togaware.com>
2023-08-04 09:30:34 +1000

    Move to my generic makefile support.

--- Makefile
+++ Makefile
@@ -1,14 +1,60 @@
-APP=Flutter Experience
+########################################################################
+#
+# Generic Makefile
+#
+# Time-stamp: <Saturday 2023-07-29 11:02:23 +1000 Graham Williams>
+#
+# Copyright (c) Graham.Williams@togaware.com
+#
+# License: Creative Commons Attribution-ShareAlike 4.0 International.
+#
+########################################################################
 
-.PHONY: analyze clean docs help linux test
+# App is often the current directory name.
+#
+# App version numbers
+#   Major release
+#   Minor update
+#   Trivial update or bug fix
+
+APP=$(shell pwd | xargs basename)
+VER=0.0.1
+DATE=$(shell date +%Y-%m-%d)
+
+########################################################################
+# Supported Makefile modules.
+
+# Often the support Makefiles will be in the local support folder, or
+# else installed in the local user's shares.
+
+INC_BASE=$(HOME)/.local/share/make
+INC_BASE=support
+
+
+# Specific Makefiles will be loaded if they are found in
+# INC_BASE. Sometimes the INC_BASE is shared by multiple local
+# Makefiles and we want to skip specific makes. Simply define the
+# appropriate INC to a non-existant location and it will be skipped.
+
+INC_DOCKER=skip
+
+# Load any modules available.
+
+INC_MODULE=$(INC_BASE)/modules.mk
+
+ifneq ("$(wildcard $(INC_MODULE))","")
+  include $(INC_MODULE)
+endif
+
+########################################################################
+# HELP
+#
+# Help for targets defined in this Makefile.
 
 define HELP
 $(APP):
 
-  analyze
-  clean
-  docs		Run `dart doc` to update doc/api/index.html.
-  linux
+  docs		Generate doc and install to ecosysl.
   test		Run the integration test suite.
 
 endef
@@ -17,27 +63,12 @@ export HELP
 help::
 	@echo "$$HELP"
 
-analyze:
-	dart analyze
+########################################################################
+# LOCAL TARGETS
 
-clean:
-	flutter clean
+locals:
+	@echo "This might be the instructions to install $(APP)"
 
-docs:
-	dart doc
-
-linux:
-	flutter run -d linux
-
-# Testing. I can have exactly the same files in test and
-# integration_test. Whenit is run from integration_test the widgets
-# get renderedd to the screen. When run from test they do not get
-# rendered and are much quicker to test! Needs more exploration.
-
-test:
-	flutter test test
-
-itest:
-	flutter test --device-id \
-	$(shell flutter devices | grep desktop | perl -pe 's|^[^•]*• ([^ ]*) .*|\1|') \
-	integration_test
+docs: doc
+	chmod -R go+rX doc
+	rsync -avzh doc/api/ root@ecosysl.net:/var/www/html/bstim/
---
+++ support/flutter.mk
@@ -0,0 +1,120 @@
+########################################################################
+#
+# Makefile template for Flutter
+#
+# Copyright 2021 (c) Graham.Williams@togaware.com
+#
+# License: Creative Commons Attribution-ShareAlike 4.0 International.
+#
+########################################################################
+
+define FLUTTER_HELP
+flutter:
+
+  android Run with an attached Android device;
+  chrome  Run with the chrome device;
+  emu	  Run with the android emulator;
+  linux   Run with the linux device;
+  doc	  Run `dart doc` to crate documentation.
+  test	  Run `flutter test` for testing.
+  itest	  Run `flutter test integration_test` for interation testing.
+
+  metrics      Run metrics over the code base:
+    nullable
+    unused_code
+    unused_files
+    analyze
+
+endef
+export FLUTTER_HELP
+
+help::
+	@echo "$$FLUTTER_HELP"
+
+.PHONY: chrome
+chrome:
+	flutter run -d chrome
+
+# 20220503 gjw The following fails if the target files already exist -
+# just needs to be run once.
+#
+# dart run build_runner build --delete-conflicting-outputs
+#
+# List the files that are automatically generated. Then they will get 
+# built as required.
+
+BUILD_RUNNER = \
+#	lib/models/synchronise_time.g.dart
+
+$(BUILD_RUNNER):
+	dart run build_runner build --delete-conflicting-outputs
+
+pubspec.lock:
+	flutter pub get
+
+.PHONY: linux
+linux: pubspec.lock $(BUILD_RUNNER)
+	flutter run -d linux
+
+.PHONY: macos
+macos: $(BUILD_RUNNER)
+	flutter run -d macos
+
+.PHONY: android
+android: $(BUILD_RUNNER)
+	flutter run -d $(shell flutter devices | grep android | cut -d " " -f 5)
+
+.PHONY: emu
+emu:
+	@if [ -n "$(shell flutter devices | grep emulator | cut -d" " -f 6)" ]; then \
+	  flutter run -d $(shell flutter devices | grep emulator | cut -d" " -f 6); \
+	else \
+	  flutter emulators --launch Pixel_3a_API_30; \
+	  echo "Emulator has been started. Rerun `make emu` to build the app."; \
+	fi
+
+.PHONY: linux_config
+linux_config:
+	flutter config --enable-linux-desktop
+
+.PHONY: metrics
+metrics: nullable unused_code unused_files analyze
+
+.PHONY: nullable
+nullable:
+	dart run dart_code_metrics:metrics check-unnecessary-nullable lib
+
+.PHONY: unused_code
+unused_code:
+	dart run dart_code_metrics:metrics check-unused-code lib
+
+.PHONY: unused_files
+unused_files:
+	dart run dart_code_metrics:metrics check-unused-files lib
+
+.PHONY: analyze
+analyze:
+	dart run dart_code_metrics:metrics analyze lib --reporter=console
+
+.PHONY: doc
+doc:
+	dart doc
+
+# Run the integration tests for the desktop device (linux, windows,
+# macos). Without this explictly specified, if I have my android
+# device connected to the computer then the testing defaults to trying
+# to install on android. 20230713 gjw
+
+.PHONY: test
+test:
+	flutter test test
+
+.PHONY: itest
+itest:
+	flutter test --device-id \
+	$(shell flutter devices | grep desktop | perl -pe 's|^[^•]*• ([^ ]*) .*|\1|') \
+	integration_test
+
+clean::
+	flutter clean
+	flutter pub get
